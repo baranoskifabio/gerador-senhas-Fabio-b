4c07a2a3fa05b2e9fd209364b5eee16627a7888f
baranoskifabio <145785098+baranoskifabio@users.noreply.github.com>
2025-08-26 08:17:31 -0300

    ..

--- main.js
+++ main.js
@@ -2,4 +2,7 @@ const numeroSenha = document.querySelector('.parametro-senha__texto')
 let tamnhoSenha = 12;
 numeroSenha.textContent = tamanhoSenha;
 
-const botoes = document.querySelectorAll('.')
+const botoes = document.querySelectorAll('.paramaro-senha__botao');
+
+botoes[0].onclick = diminuir.Tamanho;
+botoes[1]
