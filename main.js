9716de1f16f728b35cec597e34ba18e3b0a62725
baranoskifabio <145785098+baranoskifabio@users.noreply.github.com>
2025-08-26 08:36:42 -0300

    ..

--- main.js
+++ main.js
@@ -6,3 +6,22 @@ const botoes = document.querySelectorAll('.paramaro-senha__botao');
 
 botoes[0].onclick = diminuirTamanho;
 botoes[1].onclick = aumentarTamanho;
+
+function diminuirTamanho() {
+if (tamanhoSenha > 1) {
+    tamnhoSenha = tamnhoSenha - 1
+}
+numeroSenha.textContent = tamnhoSenha
+gerarSenha()
+}
+
+
+function diminuirTamanho() {
+if (tamanhoSenha < 20) {
+    tamnhoSenha = tamnhoSenha + 1
+}
+numeroSenha.textContent = tamnhoSenha
+gerarSenha()
+}
+
+const campoSenha = document.
